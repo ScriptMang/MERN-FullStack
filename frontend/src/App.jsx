d4942137213d080861488b11bba7469731c294bd
scriptMang <aperalta686@gmail.com>
2026-04-03 00:25:14 -0400

    Call the the navbar, login, register, and dashboard components with the user-context in the main app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
+import {Routes, Route, Navigate} from 'react-router-dom'
+import  {useUser} from './context/UserContext.jsx'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
-
+  const {user} = useUser()
   return (
     <>
-
+   <Navbar />
+    { user ? 
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
+    :
+   <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  }
     </>
   )
 }
