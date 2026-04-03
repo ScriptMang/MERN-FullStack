8c9bdd12b03f2ff9eb0433de8193567ebf7e9bff
scriptMang <aperalta686@gmail.com>
2026-04-03 13:10:03 -0400

    Create the project-details page

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -1,9 +1,10 @@
 import {useState, useEffect} from 'react'
 import {taskClient} from '../clients/api.js'
+import Task from '../components/Task' 
 
 function ProjectDetails(){
       const [tasks, setTasks] = useState([])
-      const [name, setName] = useState('')
+      const [title, setTitle] = useState('')
       const [description, setDescription] = useState('')
       const [status, setStatus] = useState('')
         
@@ -26,19 +27,82 @@ function ProjectDetails(){
             e.preventDefault()
     
             try {
-                // make a POST request to create the post (based off the state: name and description)
-                const { data } = await taskClient.post('/', { name, description })
+                // make a POST request to create the post (based off the state: title and desTitletion)
+                const { data } = await taskClient.post('/', { title, description, status })
             
                 // add the new post to our state
                 setTasks([data, ...tasks])
     
                 // reset the form 
-                setName('')
+                setTitle('')
                 setDescription('')
     
             } catch(err) {
                 console.log(err)
             }
         }
+
+
+ return(
+       <div>
+
+            <h1>Project Details Page</h1>
+
+            <form onSubmit={handleSubmit}>
+                <h2>Add a new task here:</h2>
+                <label htmlFor="title">Title:</label>
+                <input 
+                    type="text" 
+                    id="title"
+                    required={true}
+                    value={title}Title
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <br />
+                 <label htmlFor="status">Status:</label>
+                <input 
+                    type="text" 
+                    id="status"
+                    required={true}
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                />
+                <br />
+                <label htmlFor="description">Body:</label>
+                <textarea 
+                    type="text" 
+                    id="description"
+                    required={true}
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <br />
+                <button>Submit</button>
+            </form>
+
+            
+            {projects.map(task => { 
+                const handleTaskDelete = async (e) => {
+                    try{
+                        await projectClient.delete(`/${project._id}`)
+                        const clickedTask = task
+                        setTasks(tasks => tasks.filter(currTask => currTask._id !== clickedProj._id))
+                    } catch (e){
+                        console.log(e)                    
+                    }
+                }
+                return( 
+                <div key={task._id}>
+                    <Task task={task} />
+                    <button onClick={handleTaskDelete}>delete</button>
+               </div>
+               )
+            }
+            )}
+        </div>
+        )
+
+
+
 }
 export default ProjectDetails
