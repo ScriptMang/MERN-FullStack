73395a5a4ec89bb1b3ee17fa9cab0d0f314e33ef
scriptMang <aperalta686@gmail.com>
2026-04-03 09:51:15 -0400

    Add descriptors to  values in project component

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -1,8 +1,8 @@
 function Project({ project }) {
     return (
         <div>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
+            <h3>Project: {project.name}</h3>
+            <p>description: {project.description}</p>
         </div>
     )
 }
