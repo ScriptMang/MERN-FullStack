59e99eac86f7b95393afebe472c6c41fff974691
scriptMang <aperalta686@gmail.com>
2026-04-02 23:37:37 -0400

    Add navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import {Link} from 'react-router-dom'
+import {useUser} from '../context/UserContext.jsx'
+function Navbar(){
+    // bring in user info from context
+    const {user,logout} = useUser()
+    return(
+        <nav>
+            {user && <p>Welcome {user.username}!</p>}
+         <ul>
+            {user ?
+                <>
+                  <li> <Link to="/dashboard">Projects</Link></li>
+                  <li onClick={logout}><Link to="/login">Logout</Link></li>
+                </>
+                :
+                <>
+                 <li><Link to="/login">Login</Link></li>
+                 <li><Link to="/register">Register</Link></li>
+                </>
+            }
+         </ul>
+        </nav>
+    )
+}
+
+export default Navbar
