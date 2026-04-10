9b4bd616e0ef98c68308df5e94ecdd07ddbb3602
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2026-04-10 18:12:32 -0400

    remove unused taskroute  middleware in server file

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import './config/connection.js'
 import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
 import projectRoutes from './routes/projectRoutes.js' 
-import taskRoutes from './routes/taskRoutes.js'
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +13,5 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRoutes)
 app.use('/api/projects', projectRoutes)
-app.use('/api/tasks', taskRoutes)
 
 app.listen(port, ()=> console.log(`Listening on port: http://localhost:${port}`));
